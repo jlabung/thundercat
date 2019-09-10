7e6934762f192150b42b3b9207cbb48fc7eb3e9b
labungjugal1@gmail.com <labungjugal1@gmail.com>
2019-09-10 11:53:35 +0200

    fetch spreadsheet

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const sheetID ="spreadsheets/d/100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
+const sheetID ="100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
 
-const link = "https://spreadsheets.google.com/feeds/list/YOUR_SHEET_ID/od6/public/values?alt=json";
+const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
 
 fetch(link).then(res=>res.json()).then(showVenue);
 
 function showVenue(data){
-	console.log(data.feed.entry);
-
+	console.log(data)
 }
