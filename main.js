2544b07a57e7dc41f05fbe3b11720b08f763a92e
martin <mart12r2@stud.kea.dk>
2019-09-10 11:58:32 +0200

    data fetch

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const sheetID ="spreadsheets/d/100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
+const sheetID = "spreadsheets/d/100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
 
-const link = "https://spreadsheets.google.com/feeds/list/YOUR_SHEET_ID/od6/public/values?alt=json";
+const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
 
-fetch(link).then(res=>res.json()).then(showVenue);
+fetch(link).then(res => res.json()).then(showVenue);
 
-function showVenue(data){
+function showVenue(data) {
 	console.log(data.feed.entry);
 
 }
