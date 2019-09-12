7865cd3f3288416b4286038ec0f8588113d99b44
labungjugal1@gmail.com <labungjugal1@gmail.com>
2019-09-12 22:47:49 +0200

    initial js

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-//const button = document.querySelector("button");
-//button.addEventListener("click", () => {
-//	modal.classList.remove("hide")
-//})
-//const modal = document.querySelector(".modal-background");
-//
-//modal.addEventListener("click", () => {
-//	modal.classList.add("hide");
-//
-//	button.addEventListener("click", () => {
-//		modal.classList.remove("hide")
-//	})
-//
-//});
+
 
 const sheetID = "100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
 
@@ -32,8 +19,34 @@ function showVenue(data) {
 function showEvent(venueData) {
 	const template = document.querySelector("template").content;
 	const copy = template.cloneNode(true);
-	copy.querySelector(".data_date").textContent = venueData.gsx$address.$t;
-	console.log(venueData.gsx$address)
+
+	copy.querySelector(".venue_name").textContent = venueData.gsx$venue.$t;
+
+
+//copy.querySelector(".data_img").src = "image/"+ venueData.gsx$logo.$t + ".jpg";
+//	console.log(venueData.gsx$logo.$t)
+	copy.querySelector(".artist_name").textContent = venueData.gsx$eventname.$t;
+
+	copy.querySelector(".eventDate").textContent = venueData.gsx$eventdate.$t;
+
+	copy.querySelector(".eventDescription").textContent = venueData.gsx$description.$t;
+
+	copy.querySelector(".eventAddress").textContent = venueData.gsx$address.$t;
+
+	const button = document.querySelector("button");
+button.addEventListener("click", () => {
+	modal.classList.remove("hide")
+})
+const modal = document.querySelector(".modal-background");
+
+modal.addEventListener("click", () => {
+	modal.classList.add("hide");
+
+	button.addEventListener("click", () => {
+		modal.classList.remove("hide")
+	})
+
+});
 	document.querySelector("header").appendChild(copy);
 
 }
