0c46c7f6426a70513f6aff02f3953f0ad06e175f
labungjugal1@gmail.com <labungjugal1@gmail.com>
2019-09-12 23:08:45 +0200

    tried to put image

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ function showEvent(venueData) {
 	copy.querySelector(".venue_name").textContent = venueData.gsx$venue.$t;
 
 
-//copy.querySelector(".data_img").src = "image/"+ venueData.gsx$logo.$t + ".jpg";
-//	console.log(venueData.gsx$logo.$t)
+copy.querySelector(".data_img").src = "img/"+ venueData.gsx$logo.$t + ".jpg";	console.log(venueData.gsx$logo.$t)
 	copy.querySelector(".artist_name").textContent = venueData.gsx$eventname.$t;
 
 	copy.querySelector(".eventDate").textContent = venueData.gsx$eventdate.$t;
