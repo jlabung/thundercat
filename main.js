d7c75a5a1411c95ad8f999e1f48deaf90dd44742
martin <mart12r2@stud.kea.dk>
2019-09-12 14:28:45 +0200

    modal js

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+	modal.classList.remove("hide")
+})
+const modal = document.querySelector(".modal-background");
+
+modal.addEventListener("click", () => {
+	modal.classList.add("hide");
+
+	button.addEventListener("click", () => {
+		modal.classList.remove("hide")
+	})
+
+});
 
 
-
-
-const sheetID ="100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
-
-
-
-const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
-
-fetch(link).then(res => res.json()).then(showVenue);
-
-
-function showVenue(data){
-	console.log(data)
-	const myArray = data.feed.entry;
-	myArray.forEach(showEvent);
-}
-
-function showEvent(venueData){
-//	const template = document.querySelector("template").content;
-//	const copy = template.cloneNode(true);
-//	copy.querySelector("h1").textContent = venueData.gsx$address.$t;
-//	console.log(venueData.gsx$address)
-//	document.querySelector("header").appendChild(copy);
-}
-
+//
+//const sheetID = "100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
+//
+//
+//
+//const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
+//
+//fetch(link).then(res => res.json()).then(showVenue);
+//
+//
+//function showVenue(data) {
+//	console.log(data)
+//	const myArray = data.feed.entry;
+//	myArray.forEach(showEvent);
+//}
+//
+//function showEvent(venueData) {
+//	//	const template = document.querySelector("template").content;
+//	//	const copy = template.cloneNode(true);
+//	//	copy.querySelector("h1").textContent = venueData.gsx$address.$t;
+//	//	console.log(venueData.gsx$address)
+//	//	document.querySelector("header").appendChild(copy);
+//}
