4a30ae5fa248ca72dd797e51ca9046d8ad813b2a
labungjugal1@gmail.com <labungjugal1@gmail.com>
2019-09-13 10:54:55 +0200

    grid working

--- main.js
+++ main.js
@@ -19,11 +19,12 @@ function showVenue(data) {
 function showEvent(venueData) {
 	const template = document.querySelector("template").content;
 	const copy = template.cloneNode(true);
-
 	copy.querySelector(".venue_name").textContent = venueData.gsx$venue.$t;
 
 
-copy.querySelector(".data_img").src = "img/"+ venueData.gsx$logo.$t + ".jpg";	console.log(venueData.gsx$logo.$t)
+copy.querySelector(".data_img").src = `/img/${venueData.gsx$logo.$t}.jpg`;
+	console.log(venueData.gsx$logo.$t)
+
 	copy.querySelector(".artist_name").textContent = venueData.gsx$eventname.$t;
 
 	copy.querySelector(".eventDate").textContent = venueData.gsx$eventdate.$t;
@@ -32,20 +33,7 @@ copy.querySelector(".data_img").src = "img/"+ venueData.gsx$logo.$t + ".jpg";	co
 
 	copy.querySelector(".eventAddress").textContent = venueData.gsx$address.$t;
 
-//	const button = document.querySelector("button");
-//button.addEventListener("click", () => {
-//	modal.classList.remove("hide")
-//})
-//const modal = document.querySelector(".modal-background");
-//
-//modal.addEventListener("click", () => {
-//	modal.classList.add("hide");
-//
-//	button.addEventListener("click", () => {
-//		modal.classList.remove("hide")
-//	})
-//
-//});
-	document.querySelector("header").appendChild(copy);
+    document.querySelector(".cloneshere").appendChild(copy);
+
 
 }
