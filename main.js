53746fa47699f88b0dae53f1b6958808c6c326b7
martin <mart12r2@stud.kea.dk>
2019-09-13 09:30:24 +0200

    img

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-//const button = document.querySelector("button");
-//button.addEventListener("click", () => {
-//	modal.classList.remove("hide")
-//})
-//const modal = document.querySelector(".modal-background");
-//
-//modal.addEventListener("click", () => {
-//	modal.classList.add("hide");
-//
-//	button.addEventListener("click", () => {
-//		modal.classList.remove("hide")
-//	})
-//
-//});
+
 
 const sheetID = "100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
 
@@ -28,10 +15,43 @@ function showVenue(data) {
 	myArray.forEach(showEvent);
 }
 
+
 function showEvent(venueData) {
 	const template = document.querySelector("template").content;
 	const copy = template.cloneNode(true);
+<<<<<<< HEAD
 	copy.querySelector(".data_date").textContent = venueData.gsx$address.$t;
 	console.log(venueData.gsx$address)
 	document.querySelector(".cloneshere").appendChild(copy);
+=======
+
+	copy.querySelector(".venue_name").textContent = venueData.gsx$venue.$t;
+
+
+copy.querySelector(".data_img").src = "img/"+ venueData.gsx$logo.$t + ".jpg";	console.log(venueData.gsx$logo.$t)
+	copy.querySelector(".artist_name").textContent = venueData.gsx$eventname.$t;
+
+	copy.querySelector(".eventDate").textContent = venueData.gsx$eventdate.$t;
+
+	copy.querySelector(".eventDescription").textContent = venueData.gsx$description.$t;
+
+	copy.querySelector(".eventAddress").textContent = venueData.gsx$address.$t;
+
+//	const button = document.querySelector("button");
+//button.addEventListener("click", () => {
+//	modal.classList.remove("hide")
+//})
+//const modal = document.querySelector(".modal-background");
+//
+//modal.addEventListener("click", () => {
+//	modal.classList.add("hide");
+//
+//	button.addEventListener("click", () => {
+//		modal.classList.remove("hide")
+//	})
+//
+//});
+	document.querySelector("header").appendChild(copy);
+
+>>>>>>> origin/master
 }
