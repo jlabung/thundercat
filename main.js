3c240c7be0cd95d9d89cd4206dcc9f51711852db
labungjugal1@gmail.com <labungjugal1@gmail.com>
2019-09-17 12:11:11 +0200

    add video

--- main.js
+++ main.js
@@ -46,6 +46,9 @@ function showEvent(venueData) {
 //		fetch(`1OAaQkVzxQfBTnI-BBHuOmtLOrl_oQTBozRTsF0WvxDY`)
 		modal.classList.remove("hide");
 		modal.querySelector(".modal-image").src = "img/" + venueData.gsx$logo.$t + ".jpg";
+		modal.querySelector(".modal-name").textContent = venueData.gsx$eventname.$t;
+		modal.querySelector(".modal-description").textContent = venueData.gsx$description.$t;
+		modal.querySelector(".modal-date").textContent = venueData.gsx$eventdate.$t;
 	})
 
 	document.querySelector(".showclones").appendChild(copy);
